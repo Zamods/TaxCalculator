16eedb7bdbe3da8f2e8eb6fdaa27a716960c9d02
zamods <zamods@outlook.com>
2019-06-24 23:43:01 +0400

    -> Added Enum Converter for select Engine Displacement to calculate custom duty for new vehicles. -> Added Extensions for converting Enum to string and create selectable options. -> Added support for different languages. -> Added flex layout for floating hamburger. -> Few optimizations.

---
+++ TaxCalculator/TaxCalculator/Extensions/CultureSelector.cs
@@ -0,0 +1,34 @@
+﻿using Plugin.Multilingual;
+using System.Globalization;
+
+namespace TaxCalculator.Extensions
+{
+    public static class CultureSelector
+    {
+        public static void SelectCulture(MultilingualCulture multilingualCulture)
+        {
+            switch (multilingualCulture)
+            {
+                case MultilingualCulture.English:
+                    CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo("en");
+                    break;
+
+                case MultilingualCulture.Urdu:
+                    CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo("ur");
+                    break;
+
+                case MultilingualCulture.Default:
+                    CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo("en");
+                    break;
+            }
+            TaxCalculator.Resources.LanguageResource.Culture = CrossMultilingual.Current.CurrentCultureInfo;
+        }
+    }
+
+    public enum MultilingualCulture
+    {
+        Default,
+        English,
+        Urdu
+    };
+}
---
+++ TaxCalculator/TaxCalculator/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * https://alexdunn.org
+ * Credits : Alex Dunn
+ */
+using System.Text.RegularExpressions;
+
+namespace TaxCalculator.Extensions
+{
+    public static class StringExtension
+    {
+        public static string SplitCamelCaseToEngineDisplacementString(this string str)
+        {
+            return Regex.Replace(Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\p{Ll}\P{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2"), @"(to)", " $1"); 
+        }
+    }
+}
